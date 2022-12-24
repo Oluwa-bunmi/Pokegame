d72c6a4114ee0b93c2047655cab4e0fe0e4d5b3f
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-12-24 18:03:31 +0100

    pokedex component well styled

--- src/components/Pokecard/Pokecard.jsx
+++ src/components/Pokecard/Pokecard.jsx
@@ -8,10 +8,10 @@ const Pokecard = ({ id, name, type, exp }) => {
   let imgSrc = `${POKE_API}${id}.png`;
   return (
     <div className="Pokecard">
-      <h1>{name}</h1>
+      <h1 className="Pokecard-title">{name}</h1>
       <img src={imgSrc} alt={name} />
-      <div>Type: {type}</div>
-      <div>EXP: {exp}</div>
+      <div className="Pokecard-data">Type: {type}</div>
+      <div className="Pokecard-data">EXP: {exp}</div>
     </div>
   );
 };
--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -7,20 +7,21 @@ const Pokedex = () => {
     { id: 11, name: "Metapod", type: "bug ", base_experience: 62 },
     { id: 12, name: "Butterfree", type: "flying", base_experience: 62 },
     { id: 12, name: "Pickachu", type: "electric", base_experience: 112 },
+    { id: 12, name: "Eevee", type: "electric", base_experience: 112 },
+    { id: 12, name: "Gengar", type: "electric", base_experience: 112 },
+    { id: 12, name: "Metapod", type: "electric", base_experience: 112 },
+    { id: 12, name: "Jigglypuff", type: "electric", base_experience: 112 },
   ];
   return (
-    <div>
-      <h1>Poke</h1>
-      {data.map(({ id, name, type, base_experience }) => {
-        return (
-          <Pokecard
-            name={name}
-            id={id}
-            type={type}
-            exp={base_experience}
-          />
-        );
-      })}
+    <div className="Pokedex">
+      <h1>Pokedex</h1>
+      <div className="Pokedex-cards">
+        {data.map(({ id, name, type, base_experience }) => {
+          return (
+            <Pokecard name={name} id={id} type={type} exp={base_experience} />
+          );
+        })}
+      </div>
     </div>
   );
 };
